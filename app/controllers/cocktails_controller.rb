3223da8696600b4f255f84982e2571cb9ca2f5eb
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-04-29 22:29:10 -0400

    front end work

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,7 @@ class CocktailsController < ApplicationController
 
 
     def show()
+        @Dose = Dose.new
     end
 
 
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,24 @@
 class DosesController < ApplicationController
-
+  before_action :find_cocktail
+  
   
   def new
+    @dose = Dose.new
   end
 
   def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save!
+      redirect_to cocktail_path(@cocktail)
+    else  
+      render :new
+    end
   end
 
   def destroy
+    @dose.destroy
+    @dose.save
   end
 
 
@@ -18,4 +29,10 @@ class DosesController < ApplicationController
   def find_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
+
+  def dose_params 
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+
 end
