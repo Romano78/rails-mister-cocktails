a271d892ee34900545b49c44487916cba572bc0e
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-04-30 11:49:20 -0400

    index cards css hml done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,37 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredient_serialized = open(url).read
 ingredients = JSON.parse(ingredient_serialized)
 
+
+puts "Deleting all Doses"
+Dose.destroy_all
 puts "Deleting all Ingredient"
 Ingredient.destroy_all
 
+ingredient_list = []
+
+
+ingredients["drinks"].each do |ingredient|
+    ingredient_list << ingredient["strIngredient1"]
+end
+
 
 puts "Creating Ingredients"
-ingredients["drinks"].each do |ingredient|
-    puts "#{ingredient["strIngredient1"]}"
-    3.times do
-        Ingredient.create(
-            name: ingredient["strIngredient1"]
-        )
-    end
-    
+
+50.times do 
+   ingredient = Ingredient.create!(
+        name: ingredient_list.sample
+    )
+    puts "#{ingredient.name}"
 end
+
+
+
 
 puts "Deleting Cocktails"
 Cocktail.destroy_all
 
 puts "Creaking Cocktail"
-10.times do
+3.times do
     Cocktail.create(
         name: Faker::TvShows::SouthPark.character,
         photo_url: "https://source.unsplash.com/1600x900/?cocktails"
