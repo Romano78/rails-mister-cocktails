cdf294caf538360bb2b2a7e3f9df360a9818bacb
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-04-30 19:15:31 -0400

    implemented rating css done for show and index

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,7 +29,7 @@ class CocktailsController < ApplicationController
     private 
 
     def cocktail_params()
-        params.require(:cocktail).permit(:name)
+        params.require(:cocktail).permit(:name, :description, :rating)
     end
     
     def find_cocktail()
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,7 +3,8 @@ class Cocktail < ApplicationRecord
     has_many :ingredients, through: :doses
 
     validates :name, presence: true, uniqueness: true
-    
+    validates :description, presence: true, length: { maximum: 1000 }
+    validates :rating, presence: true, :inclusion => 1..5
 
 
 end
